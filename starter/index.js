862da3c82f5276031d10e2db59c9f9192683571e
sucad hassan <sucad1@hotmail.co.uk>
2023-03-05 22:35:52 +0000

    added intern prompt

--- starter/index.js
+++ starter/index.js
@@ -17,61 +17,96 @@ const teamMemebers = []
 
 
 const appMenu = () => {
-   
-   
-    function buildTeam(){
 
-    }
-    
-    function addIntern(){
+
+    function buildTeam() {
 
     }
 
-   function addEngineer(){
-    inquirer.prompt([
-        {
-             type: "input",    
-             name: "engineerName",
-             message:"what is the engineer's name?"
+    function addIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "what is the intern name?"
 
-        },
-        {
-            type: "input",    
-            name: "engineerId",
-            message:"what is the engineer's Id?"
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "what is the intern Id?"
 
-       },
-       {
-             type: "input",    
-             name: "engineerEmail",
-             message:"what is the engineer's email?"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "what is the intern email?"
 
-        },
-        {
-            type: "input",    
-            name: "engineerGithub",
-            message:"what is the engineer's github name?"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "what is the intern school?"
 
-       },
-    ]).then(answers => {
-        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
-        teamMemebers.push(engineer);
-        idList.push(answers.engineerId);
-        console.log(engineer);
-        createTeam()
+            },
+        ]).then(answers => {
+            const engineer = new Intern (answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            teamMemebers.push(engineer);
+            idList.push(answers.engineerId);
+            console.log(engineer);
+            createTeam()
 
-    })
+        })
 
-   }
-    
-    
-    function createTeam(){
+    }
+
+
+
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "what is the engineer's name?"
+
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "what is the engineer's Id?"
+
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "what is the engineer's email?"
+
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "what is the engineer's github name?"
+
+            },
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamMemebers.push(engineer);
+            idList.push(answers.engineerId);
+            console.log(engineer);
+            createTeam()
+
+        })
+
+    }
+
+
+    function createTeam() {
         inquirer.prompt([
             {
                 type: "list",
                 name: "memberChoice",
                 message: "Which type of team do you want to add?",
-                choices:[
+                choices: [
                     "Intern",
                     "Engineer",
                     "Not Applicable"
@@ -79,7 +114,7 @@ const appMenu = () => {
                 ]
             }
         ]).then(userChoice => {
-            if(userChoice.memberChoice === "Engineer") {
+            if (userChoice.memberChoice === "Engineer") {
                 // Engineer function
                 addEngineer();
             } else if (userChoice.memberChoice === "Intern") {
@@ -91,9 +126,9 @@ const appMenu = () => {
             }
         })
     }
-    
-    
-    function createManager(){
+
+
+    function createManager() {
         console.log("Build your team");
         inquirer.prompt([
             {
@@ -101,7 +136,7 @@ const appMenu = () => {
                 name: 'managerName',
                 message: "What is the manager's name?",
                 validate: answer => {
-                    if(answer !==""){
+                    if (answer !== "") {
                         return true
                     }
                     return "Please enter a valid name."
@@ -110,7 +145,7 @@ const appMenu = () => {
             {
                 type: "input",
                 name: "managerId",
-                message: "What is team manager's id", 
+                message: "What is team manager's id",
 
             },
             {
@@ -119,13 +154,13 @@ const appMenu = () => {
                 message: "What is the team manager's email?"
             },
             {
-                type:"input",
-                name:"managerOfficeNumber",
+                type: "input",
+                name: "managerOfficeNumber",
                 message: "What is the team manager's office number"
             },
 
         ]).then(answers => {
-            const manager = new Manager (answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
             console.log(manager);
             teamMemebers.push(manager);
             idList.push(answers.managerId);
@@ -140,5 +175,5 @@ const appMenu = () => {
 
 }
 
-appMenu ();
+appMenu();
 
